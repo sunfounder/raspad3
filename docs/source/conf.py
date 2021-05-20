c3610aa611737898a052a01026064611a3dfa084
SunFounder <support@sunfounder.com>
2021-05-20 17:35:48 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -50,5 +50,12 @@ exclude_patterns = []
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'furo'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+html_js_files = [
+    'https://ezblock.cc/readDocFile/topHead.js',
+]
+html_css_files = [
+    'https://ezblock.cc/readDocFile/topHead.css',
+]
